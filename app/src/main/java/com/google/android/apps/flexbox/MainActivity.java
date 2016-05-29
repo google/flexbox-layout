785a5ea08ab3d73a3d62f5ea34d5f2106b15fe3c
Takeshi Hagikura <shiketa1999@gmail.com>
2016-05-29 11:32:28 +0900

    Fixes typo

--- app/src/main/java/com/google/android/apps/flexbox/MainActivity.java
+++ app/src/main/java/com/google/android/apps/flexbox/MainActivity.java
@@ -88,7 +88,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initializeStringResrouces();
+        initializeStringResources();
 
         mFlexboxLayout = (FlexboxLayout) findViewById(R.id.flexbox_layout);
         mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
@@ -164,7 +164,7 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    private void initializeStringResrouces() {
+    private void initializeStringResources() {
         ROW = getString(R.string.row);
         COLUMN = getString(R.string.column);
         ROW_REVERSE = getString(R.string.row_reverse);
