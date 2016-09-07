d5a783502dfa5631b7d2cf3e7d499fcb0f02e674
Takeshi Hagikura <thagikura@google.com>
2016-09-07 15:17:09 +0900

    Add the common methods for the FlexContainer. Adds the methods that FlexboxLayout and FlexboxLayoutManager have in common in calculating the flexbox layout.
    Change-Id: Ib0e0159cf0ea2cdc4f9c38020f4b34ec5eb2e537

--- flexbox/src/main/java/com/google/android/flexbox/FlexContainer.java
+++ flexbox/src/main/java/com/google/android/flexbox/FlexContainer.java
@@ -18,11 +18,13 @@ package com.google.android.flexbox;
 
 import android.view.View;
 
+import java.util.List;
+
 /**
  * An interface that has the common behavior as the flex container such as {@link FlexboxLayout}
  * and {@link FlexboxLayoutManager}.
  */
-interface FlexContainer {
+public interface FlexContainer {
 
     /**
      * @return the number of child count contained in the flex container.
@@ -36,4 +38,98 @@ interface FlexContainer {
      * @return the view at the index
      */
     View getChildAt(int i);
+
+
+    /**
+     * Returns a View, which is reordered by taking the order attribute into account.
+     *
+     * @param i the index of the view
+     * @return the reordered view, which order attribute is taken into account.
+     * If the index is negative or out of bounds of the number of contained views,
+     * returns {@code null}.
+     * @see FlexItem#getOrder()
+     */
+    View getReorderedChildAt(int i);
+
+    /**
+     * @return the flex direction attribute of the flex container.
+     * @see FlexDirection
+     */
+    @FlexDirection
+    int getFlexDirection();
+
+    /**
+     * Sets the given flex direction attribute to the flex container.
+     *
+     * @param flexDirection the flex direction value
+     * @see FlexDirection
+     */
+    void setFlexDirection(@FlexDirection int flexDirection);
+
+    /**
+     * @return the flex wrap attribute of the flex container.
+     * @see FlexWrap
+     */
+    @FlexWrap
+    int getFlexWrap();
+
+    /**
+     * Sets the given flex wrap attribute to the flex container.
+     *
+     * @param flexWrap the flex wrap value
+     * @see FlexWrap
+     */
+    void setFlexWrap(@FlexWrap int flexWrap);
+
+    /**
+     * @return the justify content attribute of the flex container.
+     * @see JustifyContent
+     */
+    @JustifyContent
+    int getJustifyContent();
+
+    /**
+     * Sets the given justify content attribute to the flex container.
+     *
+     * @param justifyContent the justify content value
+     * @see JustifyContent
+     */
+    void setJustifyContent(@JustifyContent int justifyContent);
+
+    /**
+     * @return the align content attribute of the flex container.
+     * @see AlignContent
+     */
+    @AlignContent
+    int getAlignContent();
+
+    /**
+     * Sets the given align content attribute to the flex container.
+     *
+     * @param alignContent the align content value
+     */
+    void setAlignContent(@AlignContent int alignContent);
+
+    /**
+     * @return the align items attribute of the flex container.
+     * @see AlignItems
+     */
+    @AlignItems
+    int getAlignItems();
+
+    /**
+     * Sets the given align items attribute to the flex container.
+     *
+     * @param alignItems the align items value
+     * @see AlignItems
+     */
+    void setAlignItems(@AlignItems int alignItems);
+
+    /**
+     * @return the flex lines composing this flex container. The overridden method should return a
+     * copy of the original list excluding a dummy flex line (flex line that doesn't have any flex
+     * items in it but used for the alignment along the cross axis) so that any changes of the
+     * returned list are not reflected to the original list.
+     */
+    List<FlexLine> getFlexLines();
 }
