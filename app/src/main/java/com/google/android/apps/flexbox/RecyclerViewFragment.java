3ab6d751a1420113c502c143d4c25d33f1ae33db
Takeshi Hagikura <shiketa1999@gmail.com>
2016-09-30 14:40:40 +0900

    Extract the common logic to calculate the initial flex lines  (#128)
    Extract the common logic to calculate the initial flex lines from the
    FlexboxLayout to FlexboxHelper to re-use it from the FlexboxLayout and the
    FlexboxLayoutManager.

--- app/src/main/java/com/google/android/apps/flexbox/RecyclerViewFragment.java
+++ app/src/main/java/com/google/android/apps/flexbox/RecyclerViewFragment.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.android.apps.flexbox;
 
 import com.google.android.apps.flexbox.recyclerview.FlexItemAdapter;
@@ -64,10 +80,13 @@ public class RecyclerViewFragment extends Fragment {
             removeFab.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if (flexboxLayoutManager.getChildCount() == 0) {
+                    if (adapter.getItemCount() == 0) {
                         return;
                     }
-                    flexboxLayoutManager.removeViewAt(flexboxLayoutManager.getChildCount() - 1);
+                    adapter.removeFlexItem(adapter.getItemCount() - 1);
+
+                    // TODO: Specify index?
+                    adapter.notifyDataSetChanged();
                 }
             });
         }
