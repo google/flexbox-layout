3f0a0937dad3da31bd33ce84092e431b8e0b2604
Takeshi Hagikura <shiketa1999@gmail.com>
2016-10-21 10:48:28 +0900

    Implement rest of layoutchildren (#129)
    Implement the rest of onLayoutChildren.
    
    This PR implements the basic layout and scrolling/recycling features for only
    the case where flexDirection = ROW and flexWrap = WRAP and the vertically
    scrolling.

---
+++ flexbox/src/androidTest/java/com/google/android/flexbox/test/TestAdapter.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2016 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.flexbox.test;
+
+import com.google.android.flexbox.FlexItem;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * {@link RecyclerView.Adapter} implementation for {@link TestViewHolder}.
+ */
+class TestAdapter extends RecyclerView.Adapter<TestViewHolder> {
+
+    private List<FlexItem> mFlexItems;
+
+    TestAdapter() {
+        this(new ArrayList<FlexItem>());
+    }
+
+    TestAdapter(List<FlexItem> flexItems) {
+        mFlexItems = flexItems;
+    }
+
+    @Override
+    public TestViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recyclerview_viewholder, parent, false);
+        return new TestViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(TestViewHolder holder, int position) {
+        holder.mTextView.setText(String.valueOf(position + 1));
+        holder.mTextView.setBackgroundResource(R.drawable.flex_item_background);
+        holder.mTextView.setGravity(Gravity.CENTER);
+        holder.mItemView.setLayoutParams((RecyclerView.LayoutParams) mFlexItems.get(position));
+    }
+
+    public void addFlexItem(FlexItem flexItem) {
+        mFlexItems.add(flexItem);
+    }
+
+    public void removeFlexItem(int position) {
+        if (position < 0 || position >= mFlexItems.size()) {
+            return;
+        }
+        mFlexItems.remove(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mFlexItems.size();
+    }
+}
