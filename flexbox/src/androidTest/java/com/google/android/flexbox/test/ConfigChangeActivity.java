18c8edcf105643b95a6ddbe4f547f5ced8a4d443
Takeshi Hagikura <shiketa1999@gmail.com>
2017-02-21 15:05:37 +0900

    Discard the calculated flex lines when configration changes are handled manually. (#201)
    When configration changes are handled manually, onSaveInstanceState
    wasn't called in the FlexboxLayoutManager. Thus flex lines previously
    calculated with the prior configuration including width of the screen
    were not discarded.
    
    This PR introduces the fields of the width/height of the previous
    measurement. If the width (or height depending of the flex direction) is
    different from the previous value discard the prior flex lines.
    
    This PR also fixes the cases where width is getting changed in the multi
    window mode.

---
+++ flexbox/src/androidTest/java/com/google/android/flexbox/test/ConfigChangeActivity.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2017 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.flexbox.test;
+
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.util.Log;
+
+import com.google.android.flexbox.FlexboxLayout;
+
+/**
+ * Activity for testing the {@link FlexboxLayout} that handles configuration changes by itself
+ * instead of letting the system take care of those.
+ */
+public class ConfigChangeActivity extends Activity {
+
+    private static final String TAG = "ConfigChangeActivity";
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        Log.d(TAG, "onConfigurationChanged: " + newConfig);
+    }
+}
