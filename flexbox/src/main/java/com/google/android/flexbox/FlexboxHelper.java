db7aaf2d813edf60e21dd5bfbc8e7619b5faa843
古宏亚 <1138733232@qq.com>
2017-05-10 14:47:07 +0800

    Fix the margin value along the cross size for a flex item (#281)
    Fix item's crosssize calculate bug

--- flexbox/src/main/java/com/google/android/flexbox/FlexboxHelper.java
+++ flexbox/src/main/java/com/google/android/flexbox/FlexboxHelper.java
@@ -550,8 +550,8 @@ class FlexboxHelper {
 
             largestSizeInCross = Math.max(largestSizeInCross,
                     getViewMeasuredSizeCross(child, isMainHorizontal) +
-                            getFlexItemMarginStartMain(flexItem, isMainHorizontal) +
-                            getFlexItemMarginEndMain(flexItem, isMainHorizontal) +
+                            getFlexItemMarginStartCross(flexItem, isMainHorizontal) +
+                            getFlexItemMarginEndCross(flexItem, isMainHorizontal) +
                             mFlexContainer.getDecorationLengthCrossAxis(child));
             // Temporarily set the cross axis length as the largest child in the flexLine
             // Expand along the cross axis depending on the mAlignContent property if needed
